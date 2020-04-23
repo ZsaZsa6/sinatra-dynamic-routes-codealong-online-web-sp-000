35121daf6b63fb75db27a85673caabf58f17c058
Ronda D Cowen <cowenhome5@gmail.com>
2020-04-23 20:40:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ class App < Sinatra::Base
     @user_name = params[:name]
     "Goodbye,#{@user_name}"
   end
-  get "/multiply/:n1/:n2" do
+  get "/multiply/:num1/:num2" do
     @product = params[:num1].to_i * params[:num2].to_i
   	"#{@product}"
   end
